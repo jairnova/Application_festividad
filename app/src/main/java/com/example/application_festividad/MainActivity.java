9dcfa65bd7bd40ae62be712dd254b31d86b2f372
jairnova <31961415+jairnova@users.noreply.github.com>
2019-08-02 15:26:48 -0500

    Interfaz festividad Prueba 5

--- app/src/main/java/com/example/application_festividad/MainActivity.java
+++ app/src/main/java/com/example/application_festividad/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.application_festividad;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView id_festividad, nombre, habitos, modales, fecha_historica, lugar, fecha_inicio, fecha_finalizacion, lugar_celebran;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void registrar(View view){
+
+    }
+    public void buscar(View view){
+
+    }
+    public void modificar(View view){
+
+    }
+    public void eliminar(View view){
+
     }
 }
